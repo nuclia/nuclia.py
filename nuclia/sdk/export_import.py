ddfc0ef9f393710d105e4fde8e74573d7105e61d
Ferran Llamas <llamas.arroniz@gmail.com>
2023-11-10 09:30:05 +0100

    Improve tdqm usage

--- nuclia/sdk/export_import.py
+++ nuclia/sdk/export_import.py
@@ -1,3 +1,4 @@
+import os
 import time
 from functools import partial
 from typing import Optional
@@ -6,6 +7,7 @@ from nucliadb_models.export_import import (
     CreateExportResponse,
     CreateImportResponse,
     Status,
+    StatusResponse,
 )
 from tqdm import tqdm
 
@@ -14,6 +16,7 @@ from nuclia.lib.kb import NucliaDBClient
 
 MB = 1024 * 1024
 CHUNK_SIZE = 5 * MB
+STATUS_CHECK_INTERVAL_S = 3
 
 
 class NucliaExports:
@@ -30,6 +33,7 @@ class NucliaExports:
         resp = ndb.ndb.start_export(kbid=ndb.kbid)
         if path is None:
             return resp
+        print(f"Export for KB started. export_id={resp.export_id}")
         self.download(export_id=resp.export_id, path=path, **kwargs)
         return None
 
@@ -37,15 +41,13 @@ class NucliaExports:
     def download(self, *, export_id: str, path: str, **kwargs) -> None:
         ndb: NucliaDBClient = kwargs["ndb"]
         wait_for_finished(ndb, "export", export_id)
-        print(f"Export is ready to be downloaded.")
+        print(f"Export ready! Will be downloaded to {path}.")
         iterator = ndb.ndb.download_export(kbid=ndb.kbid, export_id=export_id)
         with open(path, "wb") as f:
             with tqdm(
-                desc=f"Downloading export {export_id} to {path}",
-                unit="B",
+                desc=f"Downloading data",
+                unit="iB",
                 unit_scale=True,
-                unit_divisor=1024,
-                miniters=1,
             ) as pbar:
                 for chunk in iterator(chunk_size=CHUNK_SIZE):
                     pbar.update(len(chunk))
@@ -65,12 +67,12 @@ class NucliaImports:
         ndb: NucliaDBClient = kwargs["ndb"]
 
         def iterator(path: str):
+            total_size = os.path.getsize(path)
             with tqdm(
-                desc=f"Uploading from {path} to import",
-                unit="B",
+                desc=f"Uploading data",
+                total=total_size,
+                unit="iB",
                 unit_scale=True,
-                unit_divisor=1024,
-                miniters=1,
             ) as pbar:
                 with open(path, "rb") as f:
                     while True:
@@ -80,16 +82,20 @@ class NucliaImports:
                         pbar.update(len(chunk))
                         yield chunk
 
+        print(f"Importing from {path}")
         response = ndb.ndb.start_import(kbid=ndb.kbid, content=iterator(path))
+
         if not sync:
             return response
         else:
-            wait_for_finished(ndb, "import", response.import_id)
+            import_id = response.import_id
+            print(f"Import task started. import_id={import_id}")
+            wait_for_finished(ndb, "import", import_id)
             print(f"Import finished!")
             return None
 
     @kb
-    def status(self, *, import_id: str, **kwargs) -> Status:
+    def status(self, *, import_id: str, **kwargs) -> StatusResponse:
         ndb: NucliaDBClient = kwargs["ndb"]
         return ndb.ndb.import_status(kbid=ndb.kbid, import_id=import_id)
 
@@ -97,16 +103,31 @@ class NucliaImports:
 def wait_for_finished(ndb: NucliaDBClient, type: str, id: str):
     if type not in ("export", "import"):
         raise ValueError(f"Unknown type {type}")
+
     if type == "export":
+        desc = f"Generating export"
         get_status = partial(ndb.ndb.export_status, kbid=ndb.kbid, export_id=id)
     else:
+        desc = f"Importing data"
         get_status = partial(ndb.ndb.import_status, kbid=ndb.kbid, import_id=id)
-    status = get_status().status
-    pbar = tqdm(
-        unit="it", desc=f"Waiting for {type} {id} to finish", miniters=1, delay=1
-    )
-    while status != Status.FINISHED:
-        assert status != Status.ERRORED, f"{type} failed"
-        pbar.update()
-        time.sleep(1)
-        status = get_status().status
+
+    resp: StatusResponse = get_status()
+    with tqdm(
+        desc=desc,
+        unit="resources",
+        total=0,
+    ) as pbar:
+        status = resp.status
+        processed = 0
+        while status != Status.FINISHED:
+            assert status != Status.ERRORED, f"{type} failed"
+
+            pbar.total = resp.total
+            delta_processed = max(0, resp.processed - processed)
+            pbar.update(delta_processed)
+            processed = resp.processed
+
+            time.sleep(STATUS_CHECK_INTERVAL_S)
+
+            resp = get_status()
+            status = resp.status
--- nuclia/tests/unit/test_export_import.py
+++ nuclia/tests/unit/test_export_import.py
@@ -1,12 +1,16 @@
-from nuclia.sdk.export_import import wait_for_finished
 from unittest.mock import Mock
-from nucliadb_models.export_import import Status
+
 import pytest
+from nucliadb_models.export_import import Status
+
+from nuclia.sdk.export_import import wait_for_finished
 
 
-class StatusResponse:
-    def __init__(self, values):
+class FakeStatusResponse:
+    def __init__(self, values, total=100, processed=10):
         self.values = values
+        self.total = total
+        self.processed = processed
 
     @property
     def status(self):
@@ -16,8 +20,12 @@ class StatusResponse:
 @pytest.fixture(scope="function")
 def ndb_sdk():
     ndb = Mock()
-    ndb.export_status = Mock(return_value=StatusResponse([Status.RUNNING, Status.FINISHED]))
-    ndb.import_status = Mock(return_value=StatusResponse([Status.RUNNING, Status.FINISHED]))
+    ndb.export_status = Mock(
+        return_value=FakeStatusResponse([Status.RUNNING, Status.FINISHED])
+    )
+    ndb.import_status = Mock(
+        return_value=FakeStatusResponse([Status.RUNNING, Status.FINISHED])
+    )
     return ndb
 
 
@@ -32,4 +40,3 @@ def ndb(ndb_sdk):
 def test_wait_for_finished(ndb):
     wait_for_finished(ndb, "export", "foo")
     wait_for_finished(ndb, "import", "foo")
-
