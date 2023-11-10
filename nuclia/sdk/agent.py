71be68ac52135cbfe82b507d085f414fdac3dd17
Ramon Navarro Bosch <ramon@nuclia.com>
2023-11-10 10:03:21 +0100

    Fixed

--- nuclia/sdk/agent.py
+++ nuclia/sdk/agent.py
@@ -2,8 +2,6 @@ from typing import List, Optional
 
 from nucliadb_models.search import ChatRequest
 
-from nuclia.sdk import NucliaPredict, NucliaSearch
-
 
 class Agent:
     prompt: str
@@ -12,31 +10,37 @@ class Agent:
     def __init__(self, prompt: str, filters: List[str]):
         self.prompt = prompt
         self.filters = filters
+        from nuclia.sdk import NucliaSearch
+
         self.search = NucliaSearch()
 
     def ask(self, text: str) -> str:
         chat_req = ChatRequest(query=text, prompt=self.prompt, filters=self.filters)
-        import pdb
-
-        pdb.set_trace()
         answer = self.search.chat(query=chat_req)
         return answer.answer.decode()
 
 
 class NucliaAgent:
     def __init__(self):
+        from nuclia.sdk import NucliaPredict
+
         self.predict = NucliaPredict()
 
     def generate_agent(self, text: str, model: Optional[str] = None) -> Agent:
         user_prompt = (
-            self.predict.generate(text, model).decode().replace("Prompt: ", "")[:-1]
+            self.predict.generate(
+                f"Define a prompt for an agent that will answer questions about {text}",
+                model,
+            )
+            .decode()
+            .replace("Prompt: ", "")[:-1]
         )
 
         agent_prompt = (
-            user_prompt
-            + " \nAnswer the following question based on the provided context: \n[START OF CONTEXT]\n{context}\n[END OF CONTEXT] Question: {question}"  # noqa
-        )
-
+            "Answer the following question based on the provided context: \n[START OF CONTEXT]\n{context}\n[END OF CONTEXT] Question: "  # noqa
+            + user_prompt
+            + " {question}"
+        )  # noqa
         tokens = self.predict.tokens(text)
         filters = []
         for token in tokens.tokens:
