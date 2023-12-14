9edd74d922b52bfc987b01ae7b5f85b3e2064934
Carmen Iniesta <c.iniesta@gmail.com>
2023-12-14 16:47:30 +0100

    fix context format and add test

--- nuclia/tests/test_nua/test_predict.py
+++ nuclia/tests/test_nua/test_predict.py
@@ -6,3 +6,16 @@ def test_predict(testing_config):
     embed = np.sentence(text="This is my text", model="multilingual-2023-02-21")
     assert embed.time > 0
     assert len(embed.data) == 768
+
+
+def test_rag(testing_config):
+    np = NucliaPredict()
+    generated = np.rag(
+        question="Which is the CEO of Nuclia?",
+        context=[
+            "Nuclia CTO is Ramon Navarro",
+            "Eudald Camprubí is CEO at the same company as Ramon Navarro",
+        ],
+        model="chatgpt",
+    )
+    assert b"Eudald" in generated
