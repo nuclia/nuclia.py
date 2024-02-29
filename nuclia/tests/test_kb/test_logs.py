7c15760f6235a529643cccdad4a1b121f22b9ec4
Eric BREHAULT <ebrehault@gmail.com>
2024-02-29 17:38:51 +0100

    add test

---
+++ nuclia/tests/test_kb/test_logs.py
@@ -0,0 +1,10 @@
+from nuclia.sdk.kb import NucliaKB
+from nuclia.tests.fixtures import IS_PROD
+
+
+def test_logs(testing_config):
+    if not IS_PROD:
+        assert True
+    nkb = NucliaKB()
+    logs = nkb.logs.get(type="SEARCH", month="2023-06")
+    assert len(logs) == 16
