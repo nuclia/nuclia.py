1616883ea4ed88df9f0aacb34db796e4cddffef0
Eric BREHAULT <ebrehault@gmail.com>
2024-03-08 09:06:07 +0100

    update nucliadb-models and sdk + add test on search

---
+++ nuclia/tests/test_kb/test_search.py
@@ -0,0 +1,31 @@
+from nuclia.sdk.search import NucliaSearch
+from nuclia.tests.fixtures import IS_PROD
+
+
+def test_find(testing_config):
+    if IS_PROD:
+        assert True
+        return
+    search = NucliaSearch()
+    results = search.find(query="Who is hedy Lamarr?")
+    assert len(results.resources.keys()) == 2
+    titles = [r.title for r in results.resources.values()]
+    assert "Lamarr Lesson plan.pdf" in titles
+
+def test_search(testing_config):
+    if IS_PROD:
+        assert True
+        return
+    search = NucliaSearch()
+    results = search.search(query="Who is hedy Lamarr?")
+    assert len(results.resources.keys()) == 2
+    titles = [r.title for r in results.resources.values()]
+    assert "Lamarr Lesson plan.pdf" in titles
+
+def test_filters(testing_config):
+    if IS_PROD:
+        assert True
+        return
+    search = NucliaSearch()
+    results = search.find(query="Who is hedy Lamarr?", filters=["/icon/application/pdf"])
+    assert len(results.resources.keys()) == 1
