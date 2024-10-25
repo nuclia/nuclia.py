3e709aa1da5dbbd0b15750f7b41e42354b92a7cf
Carles Bruguera <carlesba@gmail.com>
2024-10-25 13:31:47 +0200

    Add sdk for download activity log

--- nuclia/lib/models.py
+++ nuclia/lib/models.py
@@ -1,9 +1,16 @@
 from nuclia_models.events.activity_logs import (  # type: ignore
     ActivityLogsQueryResponse,
-    BaseConfigModel,
+    BaseConfigModel, DownloadRequest
 )
-
+from pydantic import BaseModel
 
 class ActivityLogsOutput(BaseConfigModel):
     data: list[ActivityLogsQueryResponse]  # type: ignore
     has_more: bool
+
+
+DownloadRequestOutput = type(
+    "DownloadRequestOutput",
+    (BaseModel,),
+    {"__annotations__": {name: field.annotation for name, field in DownloadRequest.model_fields.items() if name not in {"id", "query", "user_id"}}}
+)
