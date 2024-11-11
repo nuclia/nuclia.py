bdb3f1f54fa9b0abc5abefdb7ed7f664505af9b6
David Ruiz Falco <david.rf97@gmail.com>
2024-11-11 11:50:59 +0100

    add test

---
+++ nuclia/tests/test_kb/test_remi.py
@@ -0,0 +1,21 @@
+from nuclia.sdk.kb import NucliaKB
+from nuclia.tests.fixtures import IS_PROD
+from nuclia_models.events.remi import RemiQuery, ContextRelevanceQuery
+
+
+def test_remi_query(testing_config):
+    if not IS_PROD:
+        assert True
+        return
+    nkb = NucliaKB()
+    remi_query = nkb.remi.query(
+        query=RemiQuery(
+            month="2024-11",
+            context_relevance=ContextRelevanceQuery(
+                value=0, operation="gt", aggregation="average"
+            ),
+        )
+    )
+    assert len(remi_query) == 10
+    remi_event = nkb.remi.get_remi_event(event_id=remi_query.data[0].id)
+    assert remi_event is not None
