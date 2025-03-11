29c52a771b2f9ed032e5052fb88fe0160ca619db
David Ruiz Falco <david.rf97@gmail.com>
2025-03-11 11:43:59 +0100

    restore backup

--- nuclia/tests/test_kb/test_backup.py
+++ nuclia/tests/test_kb/test_backup.py
@@ -1,12 +1,15 @@
 from nuclia import sdk
-from nuclia_models.accounts.backups import BackupCreate
-from nuclia.tests.fixtures import TESTING_KBID
+from nuclia_models.accounts.backups import BackupCreate, BackupRestore
+from nuclia.tests.fixtures import TESTING_ACCOUNT_SLUG, TESTING_KBID
+import random
+import string
+from nuclia.sdk.kbs import NucliaKBS
 
 ZONE = "europe-1"
 
 
-def test_create_backup(testing_config):
-    # sdk.NucliaAccounts().default(TESTING_ACCOUNT_SLUG)
+def test_backup(testing_config):
+    sdk.NucliaAccounts().default(TESTING_ACCOUNT_SLUG)
 
     backup = sdk.NucliaBackup().create(
         backup=BackupCreate(kb_id=TESTING_KBID),
@@ -18,11 +21,17 @@ def test_create_backup(testing_config):
     backup_ids = [b.id for b in backups]
     assert backup.id in backup_ids
 
+    new_kb_slug = "".join(random.choices(string.ascii_letters, k=6))
+    new_kb = sdk.NucliaBackup().restore(restore=BackupRestore(slug=new_kb_slug))
+
+    kbs = NucliaKBS()
+    kbs.delete(id=new_kb.id)
+
     sdk.NucliaBackup().delete(id=backup.id, zone=ZONE)
 
 
 def test_delete_all_backups(testing_config):
-    # sdk.NucliaAccounts().default(TESTING_ACCOUNT_SLUG)
+    sdk.NucliaAccounts().default(TESTING_ACCOUNT_SLUG)
 
     backups = sdk.NucliaBackup().list(zone=ZONE)
     for id in backups:
