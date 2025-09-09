a837863584e812672f6b1e8882bdcf38d26f9d83
Carmen Iniesta <carmen@nuclia.com>
2025-09-09 11:06:17 +0100

    add async test

--- nuclia/tests/test_kb/test_split_strategies.py
+++ nuclia/tests/test_kb/test_split_strategies.py
@@ -1,4 +1,4 @@
-from nuclia.sdk.kb import NucliaKB
+from nuclia.sdk.kb import AsyncNucliaKB, NucliaKB
 
 
 def test_split_strategies(testing_config):
@@ -13,4 +13,19 @@ def test_split_strategies(testing_config):
     assert len(all.keys()) == 1
     nkb.split_strategies.delete(id=list(all.keys())[0])
     all = nkb.split_strategies.list()
+    assert len(all.keys()) == 0
+
+
+async def test_split_strategies_async(testing_config):
+    nkb = AsyncNucliaKB()
+    # preventive clean up
+    for id in await nkb.split_strategies.list().keys():
+        await nkb.split_strategies.delete(id=id)
+
+    # tests
+    await nkb.split_strategies.add(config={"name": "strategy1", "max_paragraph": 400})
+    all = await nkb.split_strategies.list()
+    assert len(all.keys()) == 1
+    await nkb.split_strategies.delete(id=list(all.keys())[0])
+    all = await nkb.split_strategies.list()
     assert len(all.keys()) == 0
