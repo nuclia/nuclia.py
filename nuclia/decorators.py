4e3b1e5628c90855b68119280b8a984b545ea810
Ferran Llamas <ferran@nuclia.com>
2026-01-26 11:58:34 +0100

    Reduce requests to global accounts endpoint

--- nuclia/decorators.py
+++ nuclia/decorators.py
@@ -27,7 +27,7 @@ def accounts(func):
 
     async def async_wrapper_checkout_accounts(*args, **kwargs):
         auth = get_async_auth()
-        await auth.accounts(cached=False)
+        await auth.accounts(cached=True)
         result = await func(*args, **kwargs)
         return result
 
