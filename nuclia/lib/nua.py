dfaf7fa4b219ee921b778889c255d421e90a4d28
Ramon Navarro Bosch <ramon@nuclia.com>
2026-05-12 00:45:56 +0200

    Fix delete

--- nuclia/lib/nua.py
+++ nuclia/lib/nua.py
@@ -128,7 +128,7 @@ class NuaClient:
         self,
         method: str,
         url: str,
-        output: Type[ConvertType],
+        output: Type[ConvertType] | None,
         payload: Optional[dict[Any, Any]] = None,
         extra_headers: Optional[dict[str, str]] = None,
         timeout: int = 60,
@@ -142,6 +142,8 @@ class NuaClient:
             )
         elif resp.status_code > 299:
             raise NuaAPIException(code=resp.status_code, detail=resp.content.decode())
+        if output is None:
+            return None  # type: ignore
         try:
             data = output.model_validate(resp.json())
         except Exception:
@@ -186,7 +188,7 @@ class NuaClient:
 
     def del_config_predict(self, kbid: str):
         endpoint = f"{self.url}{CONFIG}/{kbid}"
-        self._request("DELETE", endpoint, output=Empty)
+        self._request("DELETE", endpoint, output=None)
 
     def update_config_predict(self, kbid: str, config: LearningConfigurationUpdate):
         endpoint = f"{self.url}{CONFIG}/{kbid}"
@@ -527,7 +529,7 @@ class AsyncNuaClient:
         self,
         method: str,
         url: str,
-        output: Type[ConvertType],
+        output: Type[ConvertType] | None,
         payload: Optional[dict[Any, Any]] = None,
         extra_headers: Optional[dict[str, str]] = None,
         timeout: int = 60,
@@ -541,6 +543,8 @@ class AsyncNuaClient:
             )
         elif resp.status_code > 299:
             raise NuaAPIException(code=resp.status_code, detail=resp.content.decode())
+        if output is None:
+            return None  # type: ignore
         try:
             data = output.model_validate(resp.json())
         except Exception:
@@ -602,7 +606,7 @@ class AsyncNuaClient:
 
     async def del_config_predict(self, kbid: str):
         endpoint = f"{self.url}{CONFIG}/{kbid}"
-        await self._request("DELETE", endpoint, output=Empty)
+        await self._request("DELETE", endpoint, output=None)
 
     async def update_config_predict(
         self, kbid: str, config: LearningConfigurationUpdate
